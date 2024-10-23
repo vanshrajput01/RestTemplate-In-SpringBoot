8d05d566d2a28d06b57a4f7a0206d166c9eb7b06
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2024-10-23 16:26:34 +0530

    ApiService.java

---
+++ MainPackage/ApiService.java
@@ -0,0 +1,17 @@
+package com.example.demo.ApiService;
+
+public interface ApiService {
+
+	void getSingleDataUsingExchangeMethod();
+
+	void getListDataUsingExchangeMethod();
+
+	void postDataUsingExchangeMethod();
+
+	void deleteDataUsingExchangeMethod();
+
+	void updateDataUsingExchangeMethod();
+
+
+
+}
