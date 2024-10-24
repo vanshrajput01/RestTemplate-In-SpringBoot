fba039dee08a8e5cbf1c54aece21fcc6780f2235
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2024-10-24 10:20:39 +0530

    Create ApiService.java

---
+++ RestTemplateInJava/ForEntityMethodInRestTemplate/ApiService.java
@@ -0,0 +1,19 @@
+package com.example.demo.ApiService;
+
+public interface ApiService {
+
+	void getSingleRecordUsingForEntityMethod();
+
+	void getListOfRecordUsingForEntityMthod();
+
+	void postDataUsingForEntityMethod();
+
+	void updateData();
+
+	void deleteData();
+
+
+
+
+
+}
