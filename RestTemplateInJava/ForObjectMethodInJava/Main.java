3bf1d9d08d03f280255921cbc7c3ec2721deb025
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2024-10-24 11:07:18 +0530

    Main.java

---
+++ RestTemplateInJava/ForObjectMethodInJava/Main.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.demo.ApiService.ApiService;
+
+@SpringBootApplication
+public class Project01Application implements CommandLineRunner {
+
+	@Autowired
+	private ApiService apiService;
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project01Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		this.apiService.getSingleRecordUsingForObjectMethod();
+		this.apiService.getListRecordUsingForObjectMethod();
+
+	}
+
+}
